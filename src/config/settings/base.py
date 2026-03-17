7db34fdb314fbd256fdbe7a9d39e38678f058bb4
Olga Python <olga_python@proton.me>
2026-03-17 18:20:18 +0100

    update interface

--- src/config/settings/base.py
+++ src/config/settings/base.py
@@ -100,6 +100,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [BASE_DIR / "static",]
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+
 OTP_TTL = timedelta(minutes=5)
 OTP_LENGTH = 6
 
